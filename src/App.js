7ed7f9b891990661816d924ec77cd703bc76d418
RolandSamidu <samidu.appuhamy@gmail.com>
2025-02-14 00:54:32 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
+import MainContent from "./components/MainContent";
+import Home from "./pages/Home";
+import EmployeeList from "./pages/EmployeeList";
+import CreateEmployee from "./pages/CreateEmployee";
+import UpdateEmployee from "./pages/UpdateEmployee";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="flex">
+        <Sidebar />
+        <div className="flex-1">
+          <Header />
+          <MainContent>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/employees" element={<EmployeeList />} />
+              <Route path="/employees/create" element={<CreateEmployee />} />
+              <Route path="/employees/update/:id" element={<UpdateEmployee />} />
+            </Routes>
+          </MainContent>
+          <Footer />
+        </div>
+      </div>
+    </Router>
   );
 }
 
