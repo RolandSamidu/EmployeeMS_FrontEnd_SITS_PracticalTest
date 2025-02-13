8b2d32d37451a4d15cc2db84e2ce8e45cbc40015
RolandSamidu <samidu.appuhamy@gmail.com>
2025-02-14 00:37:19 +0530

    Updated Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white py-3 text-center mt-10 shadow-inner">
+      <p>&copy; 2025 Employee Management. All Rights Reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
